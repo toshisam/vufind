f1656b391495ba752d8cdd1cda759a8b1c0c9ab0
Markus Mächler <markus.maechler@bithost.ch>
2015-05-13 08:53:32 +0200

    Introduces piwik tracker object

--- module/VuFind/src/VuFind/View/Helper/Root/Piwik.php
+++ module/VuFind/src/VuFind/View/Helper/Root/Piwik.php
@@ -97,7 +97,7 @@ class Piwik extends \Zend\View\Helper\AbstractHelper
         }
 
         $inlineScript = $this->getView()->plugin('inlinescript');
-        return $inlineScript(\Zend\View\Helper\HeadScript::SCRIPT, $code, 'SET');
+        return $inlineScript(\Zend\View\Helper\HeadScript::SCRIPT, $code, 'SET')->prependFile($this->url . 'piwik.js');
     }
 
     /**
@@ -288,12 +288,12 @@ class Piwik extends \Zend\View\Helper\AbstractHelper
     protected function getOpeningTrackingCode()
     {
         return <<<EOT
-var _paq = _paq || [];
-(function(){
-_paq.push(['setSiteId', {$this->siteId}]);
-_paq.push(['setTrackerUrl', '{$this->url}piwik.php']);
-_paq.push(['setCustomUrl', location.protocol + '//'
-     + location.host + location.pathname]);
+var VuFindPiwikTracker = Piwik.getTracker();
+
+VuFindPiwikTracker.setSiteId({$this->siteId});
+VuFindPiwikTracker.setTrackerUrl('{$this->url}piwik.php');
+VuFindPiwikTracker.setCustomUrl(location.protocol + '//'
+     + location.host + location.pathname);
 
 EOT;
     }
@@ -306,11 +306,7 @@ EOT;
     protected function getClosingTrackingCode()
     {
         return <<<EOT
-_paq.push(['enableLinkTracking']);
-var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.defer=true; g.async=true;
-    g.src='{$this->url}piwik.js';
-s.parentNode.insertBefore(g,s); })();
+VuFindPiwikTracker.enableLinkTracking();
 
 EOT;
     }
@@ -335,7 +331,7 @@ EOT;
 
             $value = $escape($value);
             $code .= <<<EOT
-_paq.push(['setCustomVariable', $i, '$key', '$value', 'page']);
+VuFindPiwikTracker.setCustomVariable($i, '$key', '$value', 'page');
 
 EOT;
         }
@@ -359,7 +355,7 @@ EOT;
 
         // Use trackSiteSearch *instead* of trackPageView in searches
         return <<<EOT
-_paq.push(['trackSiteSearch', '$searchTerms', '$searchType', $resultCount]);
+VuFindPiwikTracker.trackSiteSearch('$searchTerms', '$searchType', $resultCount);
 
 EOT;
     }
@@ -372,7 +368,7 @@ EOT;
     protected function getTrackPageViewCode()
     {
         return <<<EOT
-_paq.push(['trackPageView']);
+VuFindPiwikTracker.trackPageView();
 
 EOT;
     }
