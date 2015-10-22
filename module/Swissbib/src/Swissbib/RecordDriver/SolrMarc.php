f2ea8d0ed2b988f5d4f1c5b8136e545d7f81f9d5
edelm <matthias.edel@unibas.ch>
2015-10-22 17:40:01 +0200

    Codestyle Anpassung

--- module/Swissbib/src/Swissbib/RecordDriver/SolrMarc.php
+++ module/Swissbib/src/Swissbib/RecordDriver/SolrMarc.php
@@ -785,7 +785,9 @@ class SolrMarc extends VuFindSolrMarc implements SwissbibRecordDriver
             }
 
             if ($field['union'] === 'RERO' && $field['tag'] === '856') {
-                if (isset($field['sf_u']) && preg_match('/^.*v_bcu\/media\/images/', $field['sf_u'])==1) {
+                if (isset($field['sf_u'])
+                    && preg_match('/^.*v_bcu\/media\/images/', $field['sf_u'])==1
+                ) {
                     return 'https://externalservices.swissbib.ch/services/' .
                     'ImageTransformer?imagePath=' . $field['sf_u'] . '&scale=1';
                 } elseif (isset($field['sf_u']) && preg_match(
