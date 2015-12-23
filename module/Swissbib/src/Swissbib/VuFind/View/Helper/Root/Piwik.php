3099369a273d8be251a4b9f7fb3f48de05e406c1
Markus Mächler <markus.maechler@bithost.ch>
2015-12-23 15:46:44 +0100

    Adds swissbib piwik.js loading

--- module/Swissbib/src/Swissbib/VuFind/View/Helper/Root/Piwik.php
+++ module/Swissbib/src/Swissbib/VuFind/View/Helper/Root/Piwik.php
@@ -76,6 +76,11 @@ EOT;
         VuFindPiwikTracker.enableLinkTracking();
     } catch (e) {}
 };
+(function(){
+var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.defer=true; g.async=true;
+    g.src='{$this->url}piwik.js';
+s.parentNode.insertBefore(g,s); })();
 EOT;
     }
 }
