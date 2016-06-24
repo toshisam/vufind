582dc378291d8e0ea5ffb4cd7bcabe94a83c9b5f
edelm <matthias.edel@unibas.ch>
2016-06-24 17:20:58 +0200

    external variable for localdir in sync.sh

--- cli/sync.sh
+++ cli/sync.sh
@@ -11,16 +11,11 @@ fi
 
 BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
-export VUFIND_LOCAL_DIR=$LOCAL_DIR
+if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
+   then export VUFIND_LOCAL_DIR=$LOCAL_DIR;
+   else export VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+fi
 export VUFIND_CACHE=$VUFIND_LOCAL_DIR/cache
-
-#while getopts l:rfv OPTION
-#do
-#  case $OPTION in
-#    l) VUFIND_LOCAL_DIR="$BASEDIR/${OPTARG}"
-#    ;;
-#  esac
-#done
 
 export VUFIND_LOCAL_MODULES=Swissbib
 
