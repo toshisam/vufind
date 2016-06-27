c2ce5a943e64a57dc4235a575961145691f3d5a7
edelm <matthias.edel@unibas.ch>
2016-06-27 16:07:59 +0200

    external variable for localdir in tab40import.sh

--- cli/tab40import.sh
+++ cli/tab40import.sh
@@ -1,25 +1,14 @@
 #!/bin/bash
 
-#CACHE_DIR=/usr/local/vufind/httpd/local/cache
-
-
-#if [ "$UID"  -ne 0 ]; then
-#        echo "you have to be root to use the git update script because cache will be cleared"
-#        exit 1
-#fi
-
-
 BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
-VUFIND_LOCAL_DIR="$BASEDIR/../local"
+
+if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
+   then VUFIND_LOCAL_DIR=$LOCAL_DIR;
+   else VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+fi
 
 export VUFIND_LOCAL_MODULES=Swissbib
 export VUFIND_LOCAL_DIR
-#export APPLICATION_ENV=development
 
 php $INDEX tab40import $@
-
-#su -c "php $INDEX tab40import $@" vfsb
-
-#please do not delete a directory with options -rf as root based on a relative directory! GH
-#rm -rf $CACHE_DIR/*
