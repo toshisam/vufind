d311598011e47c5e1cfb7c9f3add36c08410ff92
edelm <matthias.edel@unibas.ch>
2016-06-28 14:38:07 +0200

    fix scripts tab40import and sync

--- cli/sync.sh
+++ cli/sync.sh
@@ -12,19 +12,17 @@ fi
 BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
 if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
-   then export VUFIND_LOCAL_DIR=$LOCAL_DIR;
-   else export VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+   then VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+   else VUFIND_LOCAL_DIR=$LOCAL_DIR;
 fi
 export VUFIND_CACHE=$VUFIND_LOCAL_DIR/cache
 
 export VUFIND_LOCAL_MODULES=Swissbib
 
-#export APPLICATION_ENV=development
-
 su -c "php $INDEX libadmin sync $@" vfsb
 
 #please do not delete a directory with options -rf as root based on a relative directory! GH
-echo "Tryinig to remove local cache"
+echo "Trying to remove local cache"
 # no removal of hierarchy cache
 rm -rf $VUFIND_CACHE/searchspecs/*
 rm -rf $VUFIND_CACHE/objects/*
--- cli/tab40import.sh
+++ cli/tab40import.sh
@@ -4,8 +4,8 @@ BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
 
 if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
-   then export VUFIND_LOCAL_DIR=$LOCAL_DIR;
-   else export VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+   then export VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+   else export VUFIND_LOCAL_DIR=$LOCAL_DIR;
 fi
 
 export VUFIND_LOCAL_MODULES=Swissbib
