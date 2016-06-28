2e46a0cc78dcf0a5a56e6ca866500ece8b30a5a6
edelm <matthias.edel@unibas.ch>
2016-06-28 12:19:28 +0200

    external variable for localdir in tab40import.sh

--- cli/tab40import.sh
+++ cli/tab40import.sh
@@ -4,11 +4,10 @@ BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
 
 if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
-   then VUFIND_LOCAL_DIR=$LOCAL_DIR;
-   else VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+   then export VUFIND_LOCAL_DIR=$LOCAL_DIR;
+   else export VUFIND_LOCAL_DIR=${BASEDIR}/../local;
 fi
 
 export VUFIND_LOCAL_MODULES=Swissbib
-export VUFIND_LOCAL_DIR
 
 php $INDEX tab40import $@
