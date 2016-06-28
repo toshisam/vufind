c047456776ff9fca53feac5c93c1c1dd797f113b
edelm <matthias.edel@unibas.ch>
2016-06-28 15:04:48 +0200

    fix script  sync + params

--- cli/sync.sh
+++ cli/sync.sh
@@ -12,11 +12,11 @@ fi
 BASEDIR=$(dirname $0)
 INDEX="$BASEDIR/../public/index.php"
 if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
-   then VUFIND_LOCAL_DIR=${BASEDIR}/../local;
-   else VUFIND_LOCAL_DIR=$LOCAL_DIR;
+   then export VUFIND_LOCAL_DIR=${BASEDIR}/../local;
+   else export VUFIND_LOCAL_DIR=$LOCAL_DIR;
 fi
-export VUFIND_CACHE=$VUFIND_LOCAL_DIR/cache
 
+export VUFIND_CACHE=$VUFIND_LOCAL_DIR/cache
 export VUFIND_LOCAL_MODULES=Swissbib
 
 su -c "php $INDEX libadmin sync $@" vfsb
