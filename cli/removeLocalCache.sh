5cb542e6d5d38b7046735b6418870058e66c28c4
edelm <matthias.edel@unibas.ch>
2016-08-19 15:17:11 +0200

    make removeLocalCache.sh able to deal with custom local-dirs

--- cli/removeLocalCache.sh
+++ cli/removeLocalCache.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 #
 # Removal of local caches (except hierarchy cache)
+#
+# To specify your own LOCAL_DIR, use e.g.:
+# export LOCAL_DIR=/usr/local/vufind/httpd/local/classic/dev; sudo -E bash -c cli/removeLocalCache.sh
+#
+#
 
 VUFIND_BASE=/usr/local/vufind/httpd
-VUFIND_CACHE=$VUFIND_BASE/local/cache
-VUFIND_DEPLOY_LOG=$VUFIND_BASE/log
-TIMESTAMP=`date +%Y%m%d%H%M%S`  # seconds
-LOGFILE=$VUFIND_DEPLOY_LOG/remove.local.cache.$TIMESTAMP.log
+if [ -z "$LOCAL_DIR" ]; # if $LOCAL_DIR empty or unset, use default localdir
+    then export VUFIND_LOCAL=${VUFIND_BASE}/local;
+    else export VUFIND_LOCAL=$LOCAL_DIR;
+fi
+
+echo $LOCAL_DIR
+VUFIND_CACHE=$VUFIND_LOCAL/cache
 
 if [ "$UID"  -eq 0 ]; then
 
-    echo "Tryinig to remove local cache"
+    echo "Tryinig to remove local cache: "
+    echo $VUFIND_CACHE
     # no removal of hierarchy cache
     rm -rf $VUFIND_CACHE/searchspecs/*
     rm -rf $VUFIND_CACHE/objects/*
