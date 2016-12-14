cb970094cec4065df6c5ba8c290d008d886940e5
Guenter Hipler <guenter.hipler@unibas.ch>
2016-12-14 19:14:19 +0100

    get the lates commit before bugfix

--- module/Swissbib/src/Swissbib/View/Helper/NationalLicences.php
+++ module/Swissbib/src/Swissbib/View/Helper/NationalLicences.php
@@ -65,23 +65,30 @@ class NationalLicences extends AbstractHelper
     {
         $this->sm = $sm;
         $this->config = $sm->getServiceLocator()->get('VuFind\Config')
-            ->get('config');
+            ->get("NationalLicences");
+        $this->helperManager =  $sm->getServiceLocator()->get('viewhelpermanager');
         $this->ipMatcher = new IpMatcher();
-        if (!empty($this->config['SwissAcademicLibraries'])) {
+
+        $sectionPresent = !empty(
+            $sm->getServiceLocator()
+            ->get('VuFind\Config')->get('config')->SwissAcademicLibraries
+        );
+        if ($sectionPresent) {
             $this->validIps = explode(
-                ",", $this->config
-                    ->SwissAcademicLibraries->patterns_ip
+                ",", $sm->getServiceLocator()->get('VuFind\Config')
+                    ->get('config')->SwissAcademicLibraries->patterns_ip
             );
         }
         $this->remoteAddress = new RemoteAddress();
         $this->remoteAddress->setUseProxy();
         $trustedProxies = explode(
             ',', $sm->getServiceLocator()->get('VuFind\Config')
-            ->get('TargetsProxy')->get('TrustedProxy')->get('loadbalancer')
+                ->get('TrustedProxy')->get('loadbalancer')
         );
         $this->remoteAddress->setTrustedProxies($trustedProxies);
         $this->nationalLicenceService = $this->sm->getServiceLocator()
             ->get('Swissbib\NationalLicenceService');
+
 
         /*
         Based on Oxford mapping:
